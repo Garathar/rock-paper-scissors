65e068c9acaff9de5614189a21a7edd02490d936
Garathar <garatarganis@gmail.com>
2026-03-17 09:38:07 +0100

    Add getComputerChoice as function

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-console.log("Testing")
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*100) + 1
+    if (randomNumber < 33){
+        return "rock"
+    }
+    else if (randomNumber > 33 && randomNumber < 66){
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
